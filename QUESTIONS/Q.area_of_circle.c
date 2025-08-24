c8eab484653cfbaa0729d35e338d558bdee04de6
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-24 23:47:42 +0530

    24/aug/25 study

---
+++ QUESTIONS/Q.area_of_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+
+    int r=5;
+    float A=3.14*r*r;
+    printf ("Area of the Circle is %f",A);
+
+
+
+    return 0;
+}
---
+++ QUESTIONS/Q.volume_of_sphere.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    int r=5;
+    float V=4*3.14*r*r*r/3;
+    printf ("Volume of Sphere is=%f",V);
+
+    return 0;
+}
