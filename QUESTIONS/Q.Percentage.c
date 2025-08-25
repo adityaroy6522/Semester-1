e1d4fd9240271ae937b12c1d198240cdba28606f
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-25 09:51:03 +0530

    nothing changed much

---
+++ QUESTIONS/Q.Percentage.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    float a=1;                                    //marks of maths
+    float b=2;                                    //marks of science
+    float c=3;                                    //marks of history
+    float d=4;                                    //marks of english
+    float e=5.5;                                    //marks of hindi
+float f=(a+b+c+d+e)/5;
+printf ("persentage of 5 sub is %f",f);
+    return 0;
+}
