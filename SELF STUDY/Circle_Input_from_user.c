52cfac73423e01a78d762a7b8ce05954401064fa
adityaroy6522 <adityaroy6522@gmail.com>
2025-08-28 01:12:30 +0530

    rearranging files

--- SELF STUDY/Circle_Input_from_user.c
+++ SELF STUDY/Circle_Input_from_user.c
@@ -2,15 +2,13 @@
 int main ()
 {
     float radius;  
-    printf ("radius of the circle is ");             
-    scanf ("%f",&radius);                       //first from float radius it creates a container and scanf ask the user to fill the container
+    printf (" ");             
+    scanf ("radius of the circle is %f",&radius);                       //first from float radius it creates a container and scanf ask the user to fill the container
 
     float pi=3.1415;
     float a=pi*radius*radius;
     printf ("area of the circle is %f",a);
 
-
     return 0;
-
 
 }
