3c812aefd4a8db3f1c2f29f489450b27149af3a8
adityaroy6522 <adityaroy6522@gmail.com>
2025-09-03 22:28:23 +0530

    self study

---
+++ CLASS/name_age.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+
+    char b;
+    printf ("enter your name ");
+    scanf ("%c",&b);
+
+    int age;
+    printf ("enter your age ");
+    scanf ("%d",&age);
+
+    printf ("your name is %c\n",b);
+    printf ("your age is %d\n",age);
+    return 0;
+}
