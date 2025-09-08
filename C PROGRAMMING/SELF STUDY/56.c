898a3f1c02c2923ef5aed3f2c215b42796afdb64
adityaroy6522 <adityaroy6522@gmail.com>
2025-09-08 22:20:41 +0530

    self study

---
+++ C PROGRAMMING/SELF STUDY/56.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+if (3+2%5)
+{
+    printf ("this works");
+}
+if (0)                                    //any number except 0
+{
+    printf ("this works\n ");
+}
+if (a=10)
+{
+    printf ("even this works\n");
+}
+if ('a')
+{
+    printf ("HELLO");
+}
+return 0;
+}
---
+++ C PROGRAMMING/SELF STUDY/Loop/58.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    int x;
+    scanf ("%d",&x);
+    for (int i=1;i<=x;i++)
+    {
+        printf ("hello World\n");
+    }
+    return 0;
+}
