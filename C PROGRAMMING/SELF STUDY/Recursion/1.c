a5856ebf530854b81a4d69c03b2c53049d3441c5
adityaroy6522 <adityaroy6522@gmail.com>
2025-10-30 16:47:16 +0530

    add

--- C PROGRAMMING/SELF STUDY/Recursion/1.c
+++ C PROGRAMMING/SELF STUDY/Recursion/1.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-int main() {
-int x = 2;
-switch(x) {
-case 1:
-case 2:
-case 3: printf("One to Three"); 
-default: printf("Other");
+int factorial (int n)
+{
+    int a=1;
+    for (int i=1;i<=n;i++)
+    {
+        a=a*i;
+    }
+    return a;
 }
+int main ()
+{
+    int n;
+    scanf ("%d",&n);
+    int a = factorial (n);
+    printf ("%d",a);
+    return 0;
 }
