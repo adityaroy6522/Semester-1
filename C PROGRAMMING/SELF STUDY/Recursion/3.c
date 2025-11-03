c33933e84fb78b5fe29006ebf3d37fbf4b24cc66
adityaroy6522 <adityaroy6522@gmail.com>
2025-11-03 11:41:55 +0530

    add

--- C PROGRAMMING/SELF STUDY/Recursion/3.c
+++ C PROGRAMMING/SELF STUDY/Recursion/3.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+void dec (int n)
+{
+    if (n==0) return;
+    printf ("%d",n);
+    dec (n-1);
+    return;
+}
+int main ()
+{
+    int n;
+    scanf ("%d",&n);
+    dec (n);
+    return 0;
+}
