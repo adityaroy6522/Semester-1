16b7fab0b827418ee3bf198341a84eb1c0033ef7
adityaroy6522 <adityaroy6522@gmail.com>
2025-11-06 19:44:46 +0530

    add

--- C PROGRAMMING/SELF STUDY/Arrays/15.c
+++ C PROGRAMMING/SELF STUDY/Arrays/15.c
@@ -1,7 +1,13 @@
-/**/
-#include <stdio.h>
+#include <stdio.h> 
 int main ()
 {
-
+    int marks[10]={1,2,3,66,7,8,9,9,4,8};
+    for (int i=0;i<=9;i++)
+    {
+        if (marks[i]<30)
+        {
+            printf ("%d\n",marks[i]);
+        }
+    }
     return 0;
 }
